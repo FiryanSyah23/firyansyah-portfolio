e2214baa3a61c6bf25a3d59483707624bf5e5df0
FiryanSyah <firyansyah22@gmail.com>
2026-06-15 00:32:06 +0700

    add sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+	return [
+		{
+			url: "https://firyansyah-portfolio.vercel.app",
+			lastModified: new Date(),
+		},
+		{
+			url: "https://firyansyah-portfolio.vercel.app/projects",
+			lastModified: new Date(),
+		},
+	];
+}
